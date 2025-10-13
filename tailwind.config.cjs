cca184284107f4b14fe9d660d3300b1b4c915657
rgsurveys <rg_surveys@outlook.com>
2025-10-13 21:52:24 +0100

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#001f3f',
+        amber: '#f5a623'
+      }
+    }
+  },
+  plugins: [],
+}
