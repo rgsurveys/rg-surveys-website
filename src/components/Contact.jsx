d9a1cc3d670ff33a28e114d5011f563c75773713
rgsurveys <rg_surveys@outlook.com>
2026-01-04 22:52:40 +0000

    Update Contact email address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ export default function Contact(){
         <div className="grid md:grid-cols-2 gap-6">
           <div className="p-6 bg-white rounded-lg header-shadow">
             <h3 className="font-semibold">Get in touch</h3>
-            <p className="mt-2 text-gray-600">Email: <a href="mailto:rg_surveys@outlook.com" className="text-amber">rg_surveys@outlook.com</a></p>
+            <p className="mt-2 text-gray-600">Email: <a href="mailto:rg@rgsurvey.com" className="text-amber">rg@rgsurvey.com</a></p>
             <p className="mt-1 text-gray-600">Phone: 07851603528</p>
           </div>
         </div>
